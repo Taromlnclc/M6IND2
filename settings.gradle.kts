0a0788add53a745ffe0274a3c3514d264d9e1055
Lautaro Fernando Cornejo Arcos <ltrcornejo@gmail.com>
2023-08-31 23:28:34 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M6IND2"
+include(":app")
+ 
